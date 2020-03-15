37b525493e22b00964df5a4d52ce3c0ad8aef329
equk <equk@users.noreply.github.com>
2020-03-15 12:50:58 +0000

    added scss to netlify cms config

--- src/cms/index.js
+++ src/cms/index.js
@@ -1,6 +1,7 @@
 import CMS from 'netlify-cms-app'
 import PagePreview from './preview-templates/page-preview'
 import PostPreview from './preview-templates/post-preview'
+import '../assets/scss/init.scss'
 
 CMS.registerPreviewTemplate('pages', PagePreview)
 CMS.registerPreviewTemplate('posts', PostPreview)
