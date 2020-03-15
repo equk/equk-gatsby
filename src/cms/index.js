af7550ffbdcc59e773a6b0ff27921955ef3c7158
equk <equk@users.noreply.github.com>
2020-03-15 13:23:29 +0000

    removed scss import in cms to fix 'Error: Callback was already called.' in deploy (duplicated css runs)

--- src/cms/index.js
+++ src/cms/index.js
@@ -1,7 +1,6 @@
 import CMS from 'netlify-cms-app'
 import PagePreview from './preview-templates/page-preview'
 import PostPreview from './preview-templates/post-preview'
-import '../assets/scss/init.scss'
 
 CMS.registerPreviewTemplate('pages', PagePreview)
 CMS.registerPreviewTemplate('posts', PostPreview)
