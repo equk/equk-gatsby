8caa88afe61c895ac3ffddfb63b980d946d11221
equk <equk@users.noreply.github.com>
2020-03-16 10:49:12 +0000

    created new postbuild script to fix gatsby-plugin-netlify-cms css

---
+++ postbuild.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+/*!
+ * equk-gatsby-postbuild v1.0.0
+ *
+ * Copyright (c) 2020 B.Walden.  All rights reserved.
+ *
+ * Licensed under the MIT License
+ *
+ * (LICENSE file should be included with script)
+ *
+ */
+
+const fs = require('fs')
+
+const publicFolder = `${__dirname}/public/`
+const outFile = `${__dirname}/public/admin/cms.css`
+
+if (!fs.existsSync(publicFolder)) {
+  console.log(`ERROR: posts folder not found: ${publicFolder}`)
+  process.exit(1)
+}
+
+const css = fs.readdirSync(publicFolder).filter(fn => fn.endsWith('.css'))
+
+if (css.length === 1) {
+  console.log(`INFO: copying ${publicFolder + css[0]} to ${outFile}`)
+  fs.copyFile(publicFolder + css[0], outFile, err => {
+    if (err) throw err
+    console.log(`${publicFolder + css[0]} was copied to ${outFile}`)
+  })
+} else {
+  console.log(`ERROR: ${css.length} files found expected 1`)
+  process.exit(1)
+}
+
+process.exit(0)
