fccd722a0628b76c6062c2ee2607360e44ef1ac1
equk <equk@users.noreply.github.com>
2020-04-07 15:22:17 +0100

    created seperate disqus component

---
+++ src/components/common/Disqus.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReactDisqusComments from 'react-disqus-comments'
+import { useSiteMetadata } from '../../hooks'
+
+const DisqComments = ({ postTitle, postSlug }) => {
+  const { url, disqusShortname } = useSiteMetadata()
+  const postURL = `${url}/${postSlug}`
+
+  return (
+    <div className="comments">
+      {disqusShortname && (
+        <ReactDisqusComments shortname={disqusShortname} identifier={postURL} title={postTitle} url={postURL} />
+      )}
+    </div>
+  )
+}
+
+DisqComments.propTypes = {
+  postTitle: PropTypes.string,
+  postSlug: PropTypes.string,
+}
+
+export default DisqComments
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,2 +1,3 @@
 export { default as Tags } from './Tags'
 export { default as Comments } from './Comments'
+export { default as DisqComments } from './Disqus'
