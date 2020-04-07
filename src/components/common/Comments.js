2d7f1d407cadeb9713140e2891326cd1941949a2
equk <equk@users.noreply.github.com>
2020-04-07 15:26:09 +0100

    added gitalk to comments component

--- src/components/common/Comments.js
+++ src/components/common/Comments.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import ReactDisqusComments from 'react-disqus-comments'
+import Gitalk from 'gitalk'
 import { useSiteMetadata } from '../../hooks'
 
-const Comments = ({ postTitle, postSlug }) => {
-  const { url, disqusShortname } = useSiteMetadata()
+const Comments = ({ postSlug }) => {
+  const { url } = useSiteMetadata()
   const postURL = `${url}/${postSlug}`
+
+  useEffect(() => {
+    const GitTalkInstance = new Gitalk({
+      clientID: '4f0178686cd10054851a',
+      clientSecret: 'c9e33988e09afb94c00e85011841332a654a52aa',
+      repo: 'equk-comments',
+      owner: 'equk',
+      admin: ['equk'],
+      id: postSlug,
+      title: `Comments on '${postURL}'`,
+      distractionFreeMode: false,
+    })
+    GitTalkInstance.render('gitalk-container')
+  })
 
   return (
     <div className="comments">
-      {disqusShortname && (
-        <ReactDisqusComments shortname={disqusShortname} identifier={postURL} title={postTitle} url={postURL} />
-      )}
+      <div id="gitalk-container" />
     </div>
   )
 }
 
 Comments.propTypes = {
-  postTitle: PropTypes.string,
   postSlug: PropTypes.string,
 }
 
