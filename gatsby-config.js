fed7a8f7b6f0a18c4ae50b325976e5c4eb0cab20
equk <equk@users.noreply.github.com>
2020-04-09 18:56:16 +0100

    added themes/ dir to purgecss ignore for overrides.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -221,7 +221,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
-        ignore: ['prism/', 'fa/', 'gitalk/'],
+        ignore: ['prism/', 'fa/', 'gitalk/', 'themes/'],
         whitelistPatterns: [
           /fa/,
           /button/,
