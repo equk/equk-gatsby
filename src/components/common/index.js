ccaa1b3ffaa1f7bbb7ff2613356a120344d0fc7f
equk <equk@users.noreply.github.com>
2020-04-09 18:36:29 +0100

    added toggledark component to common

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as Tags } from './Tags'
 export { default as Comments } from './Comments'
 export { default as DisqComments } from './Disqus'
+export { default as ToggleDark } from './ToggleDark'
