261c01717c2eb83c7482e4079e297a4c9bd3ea12
equk <equk@users.noreply.github.com>
2021-03-27 15:29:45 +0000

    added eslint-disable to netlify-cms-app cms component as currently not used

--- src/cms/index.js
+++ src/cms/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/no-unresolved */
+/* to use this install netlify-cms-app */
+/* then add / comment out modulePath: `${__dirname}/src/cms/index.js`, */
+/* in gatsby-config.js */
 import CMS from 'netlify-cms-app'
 import PagePreview from './preview-templates/page-preview'
 import PostPreview from './preview-templates/post-preview'
