a4337bba5fd243a048786245d2950076d1158e78
equk <equk@users.noreply.github.com>
2021-09-30 11:19:04 +0100

    commented out unused import for netlify-cms-app in unused cms component

--- src/cms/index.ts
+++ src/cms/index.ts
@@ -2,9 +2,11 @@
 /* to use this install netlify-cms-app */
 /* then add / comment out modulePath: `${__dirname}/src/cms/index.js`, */
 /* in gatsby-config.js */
+/*
 import CMS from 'netlify-cms-app'
 import PagePreview from './preview-templates/page-preview'
 import PostPreview from './preview-templates/post-preview'
 
 CMS.registerPreviewTemplate('pages', PagePreview)
 CMS.registerPreviewTemplate('posts', PostPreview)
+*/
