8ed13742d5330523cce6b803b3845916a20c03e2
equk <equk@users.noreply.github.com>
2021-09-30 11:17:16 +0100

    fixed type error for tags prop in Tags.tsx

--- src/components/common/Tags.tsx
+++ src/components/common/Tags.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 interface TagsProps {
-  tags?: unknown[]
-  tagSlugs?: unknown[]
+  tags: any[]
+  tagSlugs?: any[]
 }
 
 const Tags = ({ tags, tagSlugs }: TagsProps) => (
