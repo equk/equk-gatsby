afb3727a7164b4867aa74d23e41aae3dad193dfe
equk <equk@users.noreply.github.com>
2023-05-25 12:22:50 +0100

    added in theme settings & fontFamily to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/templates/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}', './content/**/*.md'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: 'var(--theme-bg)',
+        textColor: 'var(--theme-text)',
+        link: 'var(--theme-link)',
+        accent: 'var(--theme-accent)',
+        'accent-2': 'var(--theme-accent-2)',
+      },
+      fontFamily: {
+        // Add any custom fonts here
+        sans: [...fontFamily.sans],
+        serif: [...fontFamily.serif],
+        mono: [...fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
