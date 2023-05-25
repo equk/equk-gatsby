c323b6054b54a7351ca81f4322e8859c5e5de754
equk <equk@users.noreply.github.com>
2023-05-25 11:58:00 +0100

    added content paths to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/templates/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}', './content/**/*.md'],
   theme: {
     extend: {},
   },
