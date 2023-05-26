b30bec0eb93b6ace3147d86bd4c38149c6a1d877
equk <equk@users.noreply.github.com>
2023-05-26 12:22:51 +0100

    changed styles of Page component to tailwind

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -11,16 +11,27 @@ function Page({ title, children, image }: PageProps) {
   return (
     <main>
       <article className="content">
-        <header className="page-full-header">
-          <h1 className="content-title">{title}</h1>
+        <header className="pb-16 pt-24">
+          <div className="space-y-4">
+            <h1 className="text-center text-5xl font-bold dark:text-gray-100">{title}</h1>
+          </div>
         </header>
+
         {image && (
-          <div className="post-feature-image">
-            <GatsbyImage image={image.childImageSharp.gatsbyImageData} objectFit="contain" alt="" />
+          <div className="hero-image">
+            <div className="hero-w">
+              <GatsbyImage image={image.childImageSharp.gatsbyImageData} objectFit="contain" alt="" />
+            </div>
           </div>
         )}
         <section className="page-full-content">
-          <div className="content-body">{children}</div>
+          <div className="post-content">
+            <div className="xl:col-span-3 xl:row-span-2 xl:pb-0">
+              <div className="prose lg:prose-lg mx-auto max-w-none">
+                <div className="content-body">{children}</div>
+              </div>
+            </div>
+          </div>
         </section>
       </article>
     </main>
