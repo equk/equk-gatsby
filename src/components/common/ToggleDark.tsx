971a7353204fcb871badda268ebfaf9cb2b42b30
equk <equk@users.noreply.github.com>
2023-05-26 16:41:17 +0100

    added mockElement to ToggleDark component to fix SSR

--- src/components/common/ToggleDark.tsx
+++ src/components/common/ToggleDark.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import useDarkMode from 'use-dark-mode'
 
+/**
+ * Mock element for SSR
+ *
+ * "document" is not available during server-side rendering.
+ */
+
+const noop = () => {}
+
+const mockElement = {
+  classList: {
+    add: noop,
+    remove: noop,
+  },
+}
+
 const sun = '/media/icons/sun.svg'
 const moon = '/media/icons/moon.svg'
 
 function ToggleDark() {
+  const docElement = (global.document && global.document.documentElement) || mockElement
   const darkMode = useDarkMode(false, {
     classNameDark: 'dark',
     classNameLight: 'light',
-    element: document.documentElement,
+    element: docElement,
   })
 
   return (
