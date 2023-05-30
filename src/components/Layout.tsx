4c97388d7b35f9c699ae21d2b9386dfd14c4a125
equk <equk@users.noreply.github.com>
2023-05-30 11:35:16 +0100

    cleaned up meta tags, removed some twitter meta tags as og is now used

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,24 +16,15 @@ function Layout({ children, title, description }: LayoutProps) {
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
+        <meta name="title" content={title} />
         <meta name="description" content={description} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:site" content="@equk_co_uk" />
-        <meta name="twitter:creator" content={TwitterCreator} />
         <meta property="og:description" content={description} />
         <meta property="og:title" content={title} />
-        <meta property="og:site_name" content="equk.co.uk" />
-        <meta content="https://equk.co.uk/media/social.png" property="og:image" />
-        <meta content="1024" property="og:image:width" />
-        <meta content="512" property="og:image:height" />
-        <meta content="https://equk.co.uk/media/social.png" name="twitter:image" />
-        <meta content="1024" name="twitter:image:width" />
-        <meta content="512" name="twitter:image:height" />
-        <meta content="https://equk.co.uk/media/social.png" property="og:image" />
-        <meta content="1024" property="og:image:width" />
-        <meta content="512" property="og:image:height" />
+        <meta property="og:image" content="https://equk.co.uk/media/social.png" />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="512" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={TwitterCreator} />
       </Helmet>
       {children}
     </div>
