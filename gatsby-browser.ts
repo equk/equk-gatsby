e5c519e769b7e18bcb84912783390717b2b60e81
equk <equk@users.noreply.github.com>
2023-05-31 13:04:32 +0100

    create canonical link & ogurl component using wrapPageElement in gatsby-ssr & gatsby-browser

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -3,4 +3,4 @@ require('./src/styles/global.css')
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 exports.onClientEntry = () => {}
 
-exports.onRouteUpdate = require('./lib/onRouteUpdate').onRouteUpdate
+exports.wrapPageElement = require('./lib/wrapPageElement').wrapPageElement
