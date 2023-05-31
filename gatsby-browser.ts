c4a4e0df5e259fe72809005f8fb2f1c2024e1b2c
equk <equk@users.noreply.github.com>
2023-05-31 12:18:10 +0100

    use DOM manipulation to modify url in canonical link & ogurl component using onRouteUpdate

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -2,3 +2,5 @@ require('./src/styles/global.css')
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 exports.onClientEntry = () => {}
+
+exports.onRouteUpdate = require('./lib/onRouteUpdate').onRouteUpdate
