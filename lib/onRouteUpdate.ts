613e5ed647eb7a2caf31d8ed370f9fc744cb5924
equk <equk@users.noreply.github.com>
2023-06-08 12:32:50 +0100

    use aria-label for checkbox elements

--- lib/onRouteUpdate.ts
+++ lib/onRouteUpdate.ts
@@ -2,6 +2,6 @@ export const onRouteUpdate = () => {
   // add label to input checkbox
   const domCheckbox = document.querySelectorAll(`input[type='checkbox']`)
   if (domCheckbox) {
-    domCheckbox.forEach((element) => element.setAttribute('label', 'checkbox'))
+    domCheckbox.forEach((element) => element.setAttribute('aria-label', 'checkbox'))
   }
 }
