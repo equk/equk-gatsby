cfef3a26351e131ba76f550099d14f67dc4714b4
equk <equk@users.noreply.github.com>
2023-06-08 12:05:09 +0100

    use DOM manipulation to add label to input checkbox in onRouteUpdate

--- lib/onRouteUpdate.ts
+++ lib/onRouteUpdate.ts
@@ -1,23 +1,7 @@
-export const onRouteUpdate = ({ location }) => {
-  const baseUrl = 'https://equk.co.uk'
-  // modify canonical link element
-  const domCanonical = document.querySelector(`link[rel='canonical']`)
-  if (domCanonical) {
-    const existingValue = domCanonical.getAttribute(`href`)
-    if (existingValue && baseUrl) {
-      let value = `${baseUrl}${location.pathname}`
-      value += location.hash
-      domCanonical.setAttribute(`href`, `${value}`)
-    }
-  }
-  // modify og:url element
-  const domOgurl = document.querySelector(`meta[property='og:url']`)
-  if (domOgurl) {
-    const existingValue = domOgurl.getAttribute(`content`)
-    if (existingValue && baseUrl) {
-      let value = `${baseUrl}${location.pathname}`
-      value += location.hash
-      domOgurl.setAttribute(`content`, `${value}`)
-    }
+export const onRouteUpdate = () => {
+  // add label to input checkbox
+  const domCheckbox = document.querySelectorAll(`input[type='checkbox']`)
+  if (domCheckbox) {
+    domCheckbox.forEach((element) => element.setAttribute('label', 'checkbox'))
   }
 }
